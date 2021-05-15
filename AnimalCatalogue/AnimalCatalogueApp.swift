dc85cf575b9af2c32715e6dc6bfd012a1c3fbefe
jundanaalbasyir <jalbasyir@gmail.com>
2021-05-15 17:33:57 +0700

    Project Starter

---
+++ AnimalCatalogue/AnimalCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCatalogueApp.swift
+//  AnimalCatalogue
+//
+//  Created by Jundana Al-Basyir on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalCatalogue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalCatalogue
+//
+//  Created by Jundana Al-Basyir on 15/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
